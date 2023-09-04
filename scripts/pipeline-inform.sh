5839674c8e495cf5910c236de45b5fac72c8bf95
costa-mandylor <duc@readydedis.com>
2023-09-04 20:15:11 +0700

    chore: setup ci/cd

--- scripts/pipeline-inform.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl --location --request POST 'https://api.telegram.org/bot5600076902:AAG36Cth8CADdnvf122vjGZy68HgHuUoCHI/sendMessage' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "chat_id": "-953629638",
-    "text": "Pipeline '$4' https://github.com/'$1'/commit/'$2'/checks/'$3'"
-}'
